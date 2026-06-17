c82b4bf4b49bb0b6c7b483b257dc3c52be9d5a15
Rui Wan <wan6@clemson.edu>
2026-06-17 17:17:29 -0400

    batch run

---
+++ run_kk.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export OMP_NUM_THREADS="${OMP_NUM_THREADS:-1}"
+
+detected_gpus="$(nvidia-smi -L 2>/dev/null | wc -l)"
+KOKKOS_GPUS="${KOKKOS_GPUS:-$detected_gpus}"
+MPI_RANKS="${MPI_RANKS:-$KOKKOS_GPUS}"
+
+exec mpirun -np "${MPI_RANKS}" ./lammps/build_kk/lmp \
+  -k on g "${KOKKOS_GPUS}" t "${OMP_NUM_THREADS}" \
+  -pk kokkos gpu/aware on newton on neigh half \
+  -in in.make_interface_bonds_kk
